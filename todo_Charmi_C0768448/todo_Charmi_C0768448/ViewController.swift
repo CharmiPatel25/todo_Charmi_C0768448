7b3815d6de34b833093be0e580bfe4c5b7dd89ad
CharmiPatel25 <C0768448@mylambton.ca>
2020-06-28 06:56:19 -0700

    add function to add new category | alerts functions to avoid adding same category

--- todo_Charmi_C0768448/todo_Charmi_C0768448/ViewController.swift
+++ todo_Charmi_C0768448/todo_Charmi_C0768448/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tabelView: UITableView!
+    var categoryContext: NSManagedObjectContext!
+    var notificationArray = [Todo]()
+    
+    var categoryName = UITextField()
+    var categoryArray: [Category] = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addCategory(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: addCategoryName(textField:))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            if(self.categoryName.text!.count < 1) {
+                self.emptyFieldAlert()
+                return
+            }
+            else {
+                self.addNewCategory()
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    func addNewCategory() {
+        
+        let categoryNames = self.categoryArray.map {$0.name}
+        guard !categoryNames.contains(categoryName.text) else {self.showAlert(); return}
+        let newCategory = Category(context: self.categoryContext)
+        newCategory.name = categoryName.text!
+        self.categoryArray.append(newCategory)
+        do {
+            try categoryContext.save()
+            tabelView.reloadData()
+        } catch {
+            print("Error saving categories \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Same Category Already Exists!", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
 
-}
+    }
+    
+    func emptyFieldAlert() {
+        
+        let alert = UIAlertController(title: "Alert", message: " Category Name cannot be empty", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func addCategoryName(textField: UITextField) {
+        
+        self.categoryName = textField
+        self.categoryName.placeholder = " Please Enter Category Name"
+        
+    }
+    
+} //class end
 
