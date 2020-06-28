3c07439f9b8301b039c3f618af1fed5feafc1cc6
CharmiPatel25 <C0768448@mylambton.ca>
2020-06-28 06:25:39 -0700

    add model class todoItem

---
+++ todo_Charmi_C0768448/todo_Charmi_C0768448/TodoItem.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem.swift
+//  todo_Charmi_C0768448
+//
+//  Created by user174608 on 6/28/20.
+//  Copyright © 2020 charmi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodoItem {
+    var todoTitle: String!
+    
+}
